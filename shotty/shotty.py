9e4589d929a98a08081d01944eb90eaebc60273f
Matulillo <crodriguezhervas@gmail.com>
2020-07-25 11:55:40 +0700

    Added click function to desig cli interface and scritp shotty script modifications

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,18 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='matulillo')
+ec2 = session.resource('ec2')
+@click.command()
+
+def list_instances():
+    "List instances"
+    for i in ec2.instances.all():
+        print(','.join((
+            i.id,
+            i.instance_type,
+            i.state['Name'])))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='matulillo')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
