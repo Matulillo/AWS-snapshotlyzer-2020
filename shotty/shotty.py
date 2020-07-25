10b87dd56392a99ecf44054545dff214c2c75684
Matulillo <crodriguezhervas@gmail.com>
2020-07-25 18:31:05 +0700

    Added feature to skip if there is a snapshot in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,6 +14,11 @@ def filter_instances(project):
         instances = ec2.instances.all()
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == "pending"
+
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -117,6 +122,8 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping {0} snapshot already in progess".format(v.id))
             print("Creating snapshot {0}".format(v.id))
             v.create_snapshot(Description="Created by Carlos script")
 
