5d0e4ff0476a9c84e0431ec4aa3ffd1d4467a0a4
Matulillo <crodriguezhervas@gmail.com>
2020-07-25 18:16:36 +0700

    Added feature list all snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -130,8 +130,10 @@ def create_snapshots(project):
 @snapshots.command('list')
 @click.option('--project',default=None,
     help="Only snapshots for project (tag Project:<Name>)")
+@click.option('--all','list_all', default=False, is_flag=True,
+    help="List all snapshot for all volumes not only the most recent")
 
-def list_snapshots(project):
+def list_snapshots(project,list_all):
     "List EC2 snapshots"
 
     instances = filter_instances(project)
@@ -146,6 +148,7 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                     )))
+                if s.state == "completed" and not list_all: break
     return
 
 if __name__ == '__main__':
